ba65fad7a84c191fcd3004d8f8ddbdbc94757d29
gabryi93 <gabryi.gr@gmail.com>
2023-02-20 19:12:13 +0100

    changes in sobrenosotros

--- app/src/pages/SobreNosotros.js
+++ app/src/pages/SobreNosotros.js
@@ -10,7 +10,6 @@ function SobreNosotros() {
           <div className="card">
             <img src={logoApp2} className="card-img-top" alt="..." />
 
-<<<<<<< HEAD
             <div className="card-body h-100">
               <h5 className="card-title">
                 <b>Nuevas Tecnologías</b>
@@ -25,27 +24,6 @@ function SobreNosotros() {
         <div className="col">
           <div className="card">
             <img src={logoApp} className="card-img-top" alt="..." />
-=======
-
-
-  return <>
-    <div className="row mt-5">
-      <div className="col"><div className="card">
-        <img src={logoApp2} className="card-img-top" alt="..." />
-
-        <div className="card-body h-100">
-          <h5 className="card-title"><b>Nuevas Tecnologías</b></h5>
-          <p className="card-text">Nos basamos en las nuevas tecnologías del entorno de la programación, para ofrecerte el mejor servicio al mejor precio.</p>
-
-        </div>
-      </div></div>
-      <div className="col"><div className="card">
-        <img src={logoApp} className="card-img-top" alt="..." />
-
-        <div className="card-body h-100">
-          <h5 className="card-title"><b>Trabajamos con las mejores empresas</b></h5>
-          <p className="card-text">Comparamos los precios de las VTC más importantes de tu ciudad, para que en un solo click, puedas solicitar el servicio que más se adecue a tus necesidades.</p>
->>>>>>> 1168b0b27b8ce3590c0ad9542a8e4ab23bab7f48
 
             <div className="card-body h-100">
               <h5 className="card-title">
@@ -59,19 +37,9 @@ function SobreNosotros() {
             </div>
           </div>
         </div>
-<<<<<<< HEAD
         <div className="col">
           <div className="card">
             <img src={logoApp1} className="card-img-top" alt="..." />
-=======
-      </div></div>
-      <div className="col"><div className="card">
-        <img src={logoApp1} className="card-img-top" alt="..." />
-
-        <div className="card-body h-100">
-          <h5 className="card-title"><b>Conductores homologados</b></h5>
-          <p className="card-text">Cualquier servicio de transporte que solicites, acudirá un conductor homologado, ya que trabajamos con Uber, Cabify y Bolt. </p>
->>>>>>> 1168b0b27b8ce3590c0ad9542a8e4ab23bab7f48
 
             <div className="card-body h-100">
               <h5 className="card-title">
@@ -84,7 +52,6 @@ function SobreNosotros() {
             </div>
           </div>
         </div>
-<<<<<<< HEAD
       </div>
       <div className="divsobreNostros">
         <h1>¿Por qué elegirnos?</h1>
@@ -113,25 +80,6 @@ function SobreNosotros() {
       </div>
     </>
   );
-=======
-      </div></div>
-    </div>
-    <div className='divsobreNostros'>
-      <h1>¿Por qué elegirnos?</h1>
-      <br />
-      <p>Es muy sencillo, estás cansado de siempre estar comparando entre las 3 principales aplicaciones de servicios <b>VTC</b>, para saber que trayecto es el más barato...<br />
-        Nosotros nos encargamos de comparar los precios de los principales servicios de de <b>VTC</b>, te hacemos ahorrar esos eurillos a fin de mes, hacemos que pagues <br />
-        lo <b>JUSTO</b> por el servicio que contratas.<br />
-        Y por último, siempre lo hacemos a través de una contratación directa. Es decir tú siempre vas a contratar el servicio que elijas a través del proveedor oficial.<br />
-        Queridos usuarios os deseamos un buen trayecto, pero sobre todo haceros ahorrar todo el dinero y <b>TIEMPO</b> posible.
-
-      </p>
-    </div>
-
-  </>
-
-
->>>>>>> 1168b0b27b8ce3590c0ad9542a8e4ab23bab7f48
 }
 
 export default SobreNosotros;
